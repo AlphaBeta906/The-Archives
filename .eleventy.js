0f8ebdfa06879addcf96c95e84db889df1faed6b
AlphaBeta906 <dragonkingalyx@gmail.com>
2023-04-19 20:46:58 +0800

    Initialize website

---
+++ .eleventy.js
@@ -0,0 +1,34 @@
+module.exports = config => {
+	config.addPassthroughCopy('./src/assets/')
+	
+	config.setBrowserSyncConfig({
+        callbacks: {
+            ready: function (err, browserSync) {
+                const content_404 = fs.readFileSync('_site/404.html');
+
+                browserSync.addMiddleware("*", (req, res) => {
+                    // Provides the 404 content without redirect.
+                    res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+                    res.write(content_404);
+                    res.end();
+                });
+            },
+        },
+        ui: false,
+        ghostMode: false
+    });
+
+	return {
+        templateFormats: [
+            "md",
+            "njk",
+            "html",
+            "liquid"
+        ],
+
+        dir: {
+            input: 'src',
+            output: '_site',
+        }
+    };
+};
